7d38ec767d1c5ec4861a4fe1f23e75a73c7e3796
skyv26 <dev.aakashv@gmail.com>
2022-12-03 03:36:34 +0530

    Add method for `Like` Model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User',
   belongs_to :post
+  before_save :like_counter
+  
+  def like_counter
+    post.update(likes_counter: post.likes.all.length)
+  end
 end
