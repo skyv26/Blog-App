d745a4a3e1f2cf2f83539d2f95988935349697f3
skyv26 <dev.aakashv@gmail.com>
2022-12-03 03:35:01 +0530

    Add method for `Comment` Model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+  before_save :comment_counter
+
+  def comment_counter
+    post.update(comments_counter: post.comments.all.length)
+  end
 end
