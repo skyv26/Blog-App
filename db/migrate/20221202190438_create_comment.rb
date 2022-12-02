7882fa8998f2882c464ac5cd4a6d4a8229193534
skyv26 <dev.aakashv@gmail.com>
2022-12-03 00:35:56 +0530

    Create table for `Comment`

---
+++ db/migrate/20221202190438_create_comment.rb
@@ -0,0 +1,12 @@
+class CreateComment < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.integer :comments_counter
+      t.references :author, references: :users, null: false, foreign_key: {to_table: :users}
+      t.references :post, references: :posts, null: false, foreign_key: {to_table: :posts}
+
+      t.timestamps
+    end
+  end
+end
