1c94277d8c8eb3e1d45a1164ea792db2795425ee
skyv26 <dev.aakashv@gmail.com>
2022-12-03 03:32:49 +0530

    Add methods for `Post` Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
+  before_save :post_count
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def post_count
+    author.update(posts_counter: author.posts.all.length)
+  end
 end
