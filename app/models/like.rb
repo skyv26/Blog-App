4ab87efc88ac93a14919ff00ed333d409032a071
skyv26 <dev.aakashv@gmail.com>
2022-12-03 03:38:01 +0530

    Resolve the `linter` errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User',
+  belongs_to :author, class_name: 'User'
   belongs_to :post
   before_save :like_counter
-  
+
   def like_counter
     post.update(likes_counter: post.likes.all.length)
   end
