707c5b865992a53a7c639150cb484971c4c38484
skyv26 <dev.aakashv@gmail.com>
2022-12-03 16:22:28 +0530

    Implement unit tests for `User` Model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'Tests For #User Model' do
+    before do
+      @user = User.new(name: 'Aakash', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'My name is Aakash')
+    end
+
+    describe 'Unit Test For @user object' do
+      it 'it will check @user should not be valid' do
+        expect(@user).to_not be_valid
+      end
+
+      it 'it will check name should be "Aakash"' do
+        expect(@user.name).to eql 'Aakash'
+      end
+    end
+
+    describe 'Unit Test For @user object methods' do
+      it 'it will check @user recents post should be []' do
+        expect(@user.recent_posts.length).to eql 0
+      end
+    end
+  end
+end
