2daf4748c81ae2fed728489e5fdf938c8678108e
skyv26 <dev.aakashv@gmail.com>
2022-12-03 19:47:29 +0530

    Resolved the `linters` errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User'
   after_save :post_count
-  
-  validates :title, presence: true, length: {minimum: 1, maximum: 250}
+
+  validates :title, presence: true, length: { minimum: 1, maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Comment, type: :model do
       @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 
       @post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
-  
+
       @comment = Comment.create(post: @post, author: @user, text: 'Hi Tom!')
     end
 
