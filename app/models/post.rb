048bcc8b3f4e956353481ca0cc110553810cdb00
skyv26 <dev.aakashv@gmail.com>
2022-12-03 13:26:25 +0530

    Add the `Post` model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   before_save :post_count
 
+  validates :title, presence: true, length: {minimum: 1, maximum: 250}
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
