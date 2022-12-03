d6be1769a80d6dcbf84461c085f75ac3b1c45a52
skyv26 <dev.aakashv@gmail.com>
2022-12-03 19:32:55 +0530

    Update the migrations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User'
   after_save :post_count
-
+  
   validates :title, presence: true, length: {minimum: 1, maximum: 250}
   validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
@@ -14,6 +14,6 @@ class Post < ApplicationRecord
   end
 
   def post_count
-    author.update(posts_counter: author.posts.all.length)
+    user.update(posts_counter: author.posts.all.length)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'Tests For #User Model' do
-    before do
-      @user = User.new(name: 'Aakash', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'My name is Aakash')
+    before :each do
+      @user = User.create(name: 'Aakash', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'My name is Aakash')
     end
 
     describe 'Unit Test For @user object' do
       it 'it will check @user should not be valid' do
-        expect(@user).to_not be_valid
+        expect(@user).to be_valid
       end
 
       it 'it will check name should be "Aakash"' do
