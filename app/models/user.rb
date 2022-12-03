3c7ee4a88d170798faaa158c86313204acc8ff36
skyv26 <dev.aakashv@gmail.com>
2022-12-03 13:58:41 +0530

    Remove the bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
 
   validates :name, presence: true
-  validates posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
