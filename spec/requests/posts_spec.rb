aa03da9115a985274d7d118a66c67fb4a4fe2307
skyv26 <dev.aakashv@gmail.com>
2022-12-10 23:35:50 +0530

    Resolve linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
     it 'should be passed with status 200' do
       get '/users/45/posts'
       expect(response.status).to eql(200)
@@ -15,12 +14,11 @@ RSpec.describe "Posts", type: :request do
 
     it 'should be passed with correct placeholder text' do
       get '/users/45/posts'
-      expect(response.body).to include('List of all posts by user (45)') 
+      expect(response.body).to include('List of all posts by user (45)')
     end
   end
 
-  describe "GET /users/:user_id/posts/:id" do
-
+  describe 'GET /users/:user_id/posts/:id' do
     it 'should be passed with status 200' do
       get '/users/45/posts/50'
       expect(response.status).to eql(200)
@@ -33,7 +31,7 @@ RSpec.describe "Posts", type: :request do
 
     it 'should be passed with correct placeholder text' do
       get '/users/45/posts/50'
-      expect(response.body).to match(/User: 45, Post : 50/) 
+      expect(response.body).to match(/User: 45, Post : 50/)
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
     it 'should be passed with status 200' do
       get ''
       expect(response.status).to eql(200)
@@ -15,12 +14,11 @@ RSpec.describe "Users", type: :request do
 
     it 'should be passed with correct placeholder text' do
       get '/'
-      expect(response.body).to match(/List of All Users/) 
+      expect(response.body).to match(/List of All Users/)
     end
   end
 
-  describe "GET /users/:id" do
-
+  describe 'GET /users/:id' do
     it 'should be passed with status 200' do
       get '/users/45'
       expect(response.status).to eql(200)
@@ -33,7 +31,7 @@ RSpec.describe "Users", type: :request do
 
     it 'should be passed with correct placeholder text' do
       get '/users/45'
-      expect(response.body).to match(/45/) 
+      expect(response.body).to match(/45/)
     end
   end
 end
