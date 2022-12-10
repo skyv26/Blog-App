e23312b473c155dc7cf514da0382229375de61c6
skyv26 <dev.aakashv@gmail.com>
2022-12-10 18:00:10 +0530

    Add the `custom-routes` for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
+
+  get 'users/id' => 'users#user_id'
+  get 'users/id/posts' => 'users#posts'
+  get 'users/id/posts/id' => 'users#post_id'
 end
