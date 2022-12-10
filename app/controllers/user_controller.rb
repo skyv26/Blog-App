47d5e9cd28fde153337fa517434a5f2f2ee1d24a
skyv26 <dev.aakashv@gmail.com>
2022-12-10 18:09:08 +0530

    update the `view` and `controller` of users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def index
   end
 
-  def id
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  get 'users/id' => 'users#user_id'
-  get 'users/id/posts' => 'users#posts'
-  get 'users/id/posts/id' => 'users#post_id'
+  get 'users/:id' => 'users#show'
+  # get 'users/:id/posts' => 'users#posts'
+  # get 'users/:id/posts/:id' => 'users#post_id'
 end
