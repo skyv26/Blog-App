0efe62a0163ddd5171a18a817f7bebcf98ad2372
skyv26 <dev.aakashv@gmail.com>
2022-12-10 23:13:08 +0530

    Add test for placeholder test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
+
     it 'should be passed with status 200' do
       get ''
       expect(response.status).to eql(200)
@@ -10,6 +11,11 @@ RSpec.describe "Users", type: :request do
     it 'should be passed with correct template' do
       get '/'
       expect(response.status).to render_template(:index)
+    end
+
+    it 'should be passed with correct placeholder text' do
+      get '/'
+      expect(response.body).to match(/List of All Users/) 
     end
   end
 end
