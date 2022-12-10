1ea70fb7a26d8fe0362510de650b75f4cb1ad8e2
skyv26 <dev.aakashv@gmail.com>
2022-12-10 18:42:19 +0530

    update the files by passing correct parameter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,5 @@
 class PostsController < ApplicationController
+  def index
+    @user_id = request.parameters[:user_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "users#index"
 
+  # Users
   get 'users/:id' => 'users#show'
-  # get 'users/:id/posts' => 'users#posts'
+
+  # Posts
+  get 'users/:user_id/posts' => 'posts#index'
   # get 'users/:id/posts/:id' => 'users#post_id'
 end
