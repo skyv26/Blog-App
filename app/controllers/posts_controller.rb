4cce8a5eec14247ce749e2f8021511102040b298
skyv26 <dev.aakashv@gmail.com>
2022-12-10 18:49:43 +0530

    Update the controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     @user_id = request.parameters[:user_id]
   end
+
+  def show
+    @user_id = request.parameters[:user_id]
+    @post_id = request.parameters[:id]
+  end
 end
