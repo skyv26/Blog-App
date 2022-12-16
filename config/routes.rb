92c5a6cd305c24a42820750cce2554135ff04d97
skyv26 <dev.aakashv@gmail.com>
2022-12-16 18:36:11 +0530

    add the `alias` for GET Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
 
   # Users
-  get 'users/:id' => 'users#show'
+  get 'users/:id' => 'users#show', as: :user_id
 
   # Posts
   get 'users/:user_id/posts' => 'posts#index'
