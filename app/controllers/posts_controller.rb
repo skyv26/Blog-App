3f591a38b0722ccb6a3669cd1adc1a5824942494
skyv26 <dev.aakashv@gmail.com>
2022-12-17 22:56:59 +0530

    Add the `post` functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,19 @@ class PostsController < ApplicationController
     @user = User.find(request.parameters[:user_id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(author: current_user, title: params[:title], text: params[:text])
+    if @post.save
+      redirect_to start_path
+    else
+      puts @post.errors.full_messages
+    end
+  end
+
   def show
     @post = Post.find(request.parameters[:id])
   end
