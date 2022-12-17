6ad97dd8ae1dec50f11f0f99c4a0988dd6bad6e1
skyv26 <dev.aakashv@gmail.com>
2022-12-17 22:12:11 +0530

    add the validation for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post, class_name: 'Post'
   after_save :comment_counter
 
+  validates :text, presence: true, length: { minimum: 1, maximum: 250 }
+
   def comment_counter
     post.update(comments_counter: post.comments.all.length)
   end
