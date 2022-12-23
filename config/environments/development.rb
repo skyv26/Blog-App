1a64d8f619feba4c9924e408a7593b0f9ba84726
Bantealem <bantem2021@gmail.com>
2022-12-23 18:46:18 +0300

    generate `devise` configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -77,3 +79,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
