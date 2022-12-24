8284e20a27b12dc017e605f785b9ad421942452c
skyv26 <dev.aakashv@gmail.com>
2022-12-25 00:41:47 +0530

    Resolve `jwt` errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,14 @@
-class UsersController < ApplicationController
-  def index
-    @all_users = User.all
-    @user = current_user
-  end
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @all_users = User.all
+        @user = current_user
+      end
 
-  def show
-    @user = User.find(request.parameters[:id])
+      def show
+        @user = User.find(request.parameters[:id])
+      end
+    end
   end
 end
