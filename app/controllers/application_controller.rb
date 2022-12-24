79cf374d5eca364220a3f8934563f7110480c4b8
skyv26 <dev.aakashv@gmail.com>
2022-12-25 00:47:39 +0530

    Remove `reduntant` code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  protect_from_forgery Prepend: true
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
