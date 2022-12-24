3b994971237fd17b9402bafa2e02105706ebc403
skyv26 <dev.aakashv@gmail.com>
2022-12-24 20:25:22 +0530

    Add the post delete functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,8 @@ class CommentsController < ApplicationController
     @comment = Comment.new(post_id: params[:id], author: current_user, text: params[:text])
     redirect_to post_id_path(current_user.id, params[:id]) if @comment.save
   end
+
+  def destroy
+    puts 'Aakash'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(request.parameters[:id].to_i)
+    @user = User.find(request.parameters[:id])
   end
 end
