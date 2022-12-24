b00b9307b7760338bc8e950e4d052e81e7c3fb53
Bantealem <bantem2021@gmail.com>
2022-12-24 14:29:17 +0300

    Add assign the roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.is?(:admin)
+      can :manage, :all
+    else
+      can :read, :all
+      can :destroy, Post, author: user
+      can :destroy, Comment, author: user
+    end
+
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
